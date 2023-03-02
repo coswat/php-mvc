71240305e281132316043210352b7e89e0479b99
coswat <coswat.co>
2023-03-03 03:38:34 +0530

    changed folder structure

--- public/index.php
+++ public/index.php
@@ -1,35 +1,7 @@
 <?php
-
 define("PATH", __DIR__.'/../');
 require __DIR__ . "/../vendor/autoload.php";
 
-use Core\Router as Route;
-use App\Controllers\TestController;
-use Core\View;
-
-/*Routes Pages here you can set route, supported methods GET,POST,PUT,PATCH,DELETE only examples route are given below, available functions abort(int status code) this will return the status code and show the 404 page from app/views/errors/code.php 404 is set as default,
-second function route(string name) if you set route a name , this function will return its url, third function is view(string name) this will load the view file from app/view/name.view.php 
-
-Note : layouts only works in returinig view from controller, more features add soon*/
-
-Route::get("/", function () {
-    return view("welcome");
-})->name("home");
-
-Route::get("/test", function () {
-    return "hello";
-});
-
-Route::post("/post", [TestController::class, "store"])->name("post");
-
-Route::get("/abort", function () {
-    return abort(404);
-});
-
-/*
-Route::put('/put',[Controller::class,'put'])->name('put');
-Route::patch('/patch',[Controller::class,'patch'])->name('patch');
-Route::delete('/delete',[Controller::class,'delete'])->name('delete');
-*/
+require PATH.'routes/web.php';
 
 echo runRoutes();
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Core\Router as Route;
+use App\Controllers\TestController;
+use Core\View;
+
+/*Routes Pages here you can set route, supported methods GET,POST,PUT,PATCH,DELETE only examples route are given below, available functions abort(int status code) this will return the status code and show the 404 page from app/views/errors/code.php 404 is set as default,
+second function route(string name) if you set route a name , this function will return its url, third function is view(string name) this will load the view file from app/view/name.view.php 
+
+Note : layouts only works in returinig view from controller, more features add soon*/
+
+Route::get("/", function () {
+    return view("welcome");
+})->name("home");
+
+Route::get("/test", function () {
+    return "hello";
+});
+
+Route::post("/post", [TestController::class, "store"])->name("post");
+
+Route::get("/abort", function () {
+    return abort(404);
+});
